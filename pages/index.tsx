f14c2bad28d336ef5275f55c01a12cf57907e63b
Jaesung Hong <jezsung@gmail.com>
2023-02-05 11:33:50 +0900

    add url shortening functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,36 @@
+import { useState } from 'react';
+
 export default function Home() {
-  return <></>;
+  const [url, setUrl] = useState('');
+  const [shortenedUrl, setShortenedUrl] = useState('');
+
+  return (
+    <>
+      <h1>Paste your URL to be shortened</h1>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const response = await fetch('http://localhost:3000/links', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              url: url,
+            }),
+          });
+          const data = await response.json();
+          setShortenedUrl(data['shortenedUrl']);
+        }}
+      >
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button type="submit">Convert</button>
+      </form>
+      <div>{shortenedUrl}</div>
+    </>
+  );
 }
